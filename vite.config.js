4bf57fb3c50227d257c07c9b8d2b91e3d7e08f1a
Illia Dovhaliuk <2201illia@gmail.com>
2023-11-25 05:39:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,17 @@ import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
   root: 'src',
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
   build: {
     rollupOptions: {
       input: glob.sync('./src/*.html'),
     },
     outDir: '../dist',
+    target: 'esnext',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
